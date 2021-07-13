6625a828602a7b609bb4935efff45a85c599ed55
A-Tishin <atisins@outlook.com>
2021-07-13 13:57:17 +0300

    Some basic C# stuff

---
+++ Basics/AddNumbers-02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AddNumbers_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(2, 7, 1));
+            Console.ReadKey();
+        }
+
+        static int Sum (int a, int b, int c)
+        {
+            return a + b + c;    
+        }
+    }
+}
---
+++ Basics/ConvertCurrency-14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConvertCurrency_14
+{
+    class Program
+    {
+
+        /**
+         * Currency Formatting
+         *
+         * The business is now breaking into the Brazilian market
+         * Write a new function for converting to the Brazilian real
+         * They have also decided that they should add a 1% fee to all foreign transactions
+         * Find a way to add 1% to all currency conversions (think about the DRY principle)
+         */
+
+        static void Main(string[] args)
+        {
+
+            var product = "learning C sharp";
+            var fee = 1.01;
+            var price = 12.5;
+            var priceInUSD = ConvertToUSD(price, fee);
+            var priceInBRL = ConvertToBRL(price, fee);
+            
+
+            Console.WriteLine("Product: " + product);
+            Console.WriteLine("Price in USD: " + priceInUSD);
+            Console.WriteLine("Price in BRL: " + priceInBRL);
+            Console.ReadKey();
+        }
+
+        //change this method accordingly
+        static double ConvertToUSD(double price, double fee)
+        {
+            return Math.Round(price * 1.4 * fee, 2);
+        }
+
+        //change this method accordingly
+        static double ConvertToBRL(double price, double fee)
+        {
+            return Math.Round(price * 6.13 * fee, 2);
+        }
+    }
+}
