32ba1529fb92aeae30979080881cab9be0936a77
A-Tishin <atisins@outlook.com>
2021-07-16 09:55:40 +0300

    FlowControl to check

--- FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -3,25 +3,26 @@
 namespace LargestNumber
 {
     class Program
-    {
-        //TODO: Write a C# program to to find the largest of three numbers.
+    { 
         static void Main(string[] args)
         {
             Console.WriteLine("Input the 1st number: ");
             var input1 = Console.ReadLine();
 
             Console.WriteLine("Input the 2nd number: ");
-            var input2 = Console.ReadLine();
+            var input2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Input the 3rd number: ");
-            var input3 = Console.ReadLine();
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+            var input3 = Convert.ToInt32(Console.ReadLine());
+
+            int largest = Convert.ToInt32(input1);
+            if (input2 > largest && input3 < input2)
+                largest = input2;
+            else if (input3 > largest)
+                largest = input3;
+
+            Console.WriteLine($"Largest is {largest}");
+            Console.ReadKey();
         }
     }
 }
