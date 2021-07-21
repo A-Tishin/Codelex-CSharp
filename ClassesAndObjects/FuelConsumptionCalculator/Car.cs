0ff903abfb75a763df489be8115eed312b8dd15c
A-Tishin <atisins@outlook.com>
2021-07-21 20:30:29 +0300

    fuelConsumption commit

--- ClassesAndObjects/FuelConsumptionCalculator/Car.cs
+++ ClassesAndObjects/FuelConsumptionCalculator/Car.cs
@@ -1,33 +1,37 @@
-﻿namespace FuelConsumptionCalculator
+﻿using System;
+
+namespace FuelConsumptionCalculator
 {
     public class Car
     {
+        private double _startKilometers;
+        private double _endKilometers;
+        private double _liters;
+
         public Car(double startOdo)
-        {          
+        {
+            _startKilometers = startOdo;
         }
 
         public double CalculateConsumption()
         {
-            return 0;
-        }
-
-        private double ConsumptionPer100Km()
-        {
-            return 0;
+            return Math.Round((_liters / (_endKilometers - _startKilometers) * 100), 2);
         }
 
         public bool GasHog()
         {
-            return ConsumptionPer100Km() > 15;
+            return CalculateConsumption() >= 15;
         }
 
         public bool EconomyCar()
         {
-            return ConsumptionPer100Km() < 5;
+            return CalculateConsumption() <= 5;
         }
 
         public void FillUp(int mileage, double liters)
         {
+            _endKilometers = mileage;
+            _liters = liters;
         }
     }
 }
