253f44fb30c9de322a652d422c5b147d3185f837
A-Tishin <atisins@outlook.com>
2021-07-21 18:34:56 +0300

    forgotten one

---
+++ ClassesAndObjects/ClassProduct/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassProduct1
+{
+    class Product
+    {
+        private string _name;
+        private decimal _price;
+        private int _amount;
+
+        public Product(string name, decimal priceAtStart, int amountAtStart)
+        {
+            _name = name;
+            _amount = amountAtStart;
+            _price = priceAtStart;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name} | price: {_price} | quantity: {_amount}");
+            Console.WriteLine();
+        }
+
+        public void NewPrice(decimal price)
+        {
+            _price = price;
+        }
+
+        public void NewQuantity()
+        {
+            _amount--;
+        }
+    }
+}
---
+++ ClassesAndObjects/ClassProduct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ClassProduct1;
+
+namespace ClassProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product("Logitech mouse", (decimal)70.00, 14);
+            Product iphone = new Product("Iphone", (decimal)999.99, 3);
+            Product printer = new Product("Epson EB-U05", (decimal)440.46, 1);
+
+            mouse.PrintProduct();
+            iphone.PrintProduct();
+            printer.PrintProduct();
+
+            Console.WriteLine("Please, make mouse cheaper.");
+            mouse.NewPrice(Convert.ToDecimal(Console.ReadLine()));
+            Console.WriteLine("And buy it!(y)");
+
+            if (Console.Read() == 'y')
+            {
+                mouse.NewQuantity();
+            }
+
+            mouse.PrintProduct();
+        }
+    }
+}
