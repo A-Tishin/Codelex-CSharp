730217253c0ac393b53e113ed42b48c546cd61a4
A-Tishin <atisins@outlook.com>
2021-07-31 09:27:19 +0300

    4/6 done

---
+++ Polymorphism/Persons/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persons
+{
+    class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _adress;
+        private int _id;
+
+        protected Person(string name, string lastName, string adress, int id)
+        {
+            _firstName = name;
+            _lastName = lastName;
+            _adress = adress;
+            _id = id;
+        }
+
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+        public string Adress => _adress;
+        public int Id => _id;
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"{_firstName} {_lastName}, lives at {_adress}. ID: {_id}");
+        }
+    }
+}
---
+++ Polymorphism/Persons/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    class Student: Person
+    {
+        private double _gpa;
+
+        public Student(string name, string lastName, string adress, int id, double gpa)
+            : base(name, lastName, adress, id)
+
+        {
+            _gpa = gpa;
+        }
+
+        public double Gpa => _gpa;
+
+        public override void Display()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, lives at {Adress}, have {Gpa} points. ID: {Id}");
+        }
+    }
+}
